003e16effc8ad9cb08ddbe21e14a5b8076bae3dd
OlehZherebetskiy <olehvel@gmail.com>
2021-02-26 08:46:40 +0200

    add lab1

--- 3 course/ParOb/lab1.cpp
+++ 3 course/ParOb/lab1.cpp
@@ -25,12 +25,14 @@ int main(int argc, char* argv[]) {
 
         for (int threadNum = 1; threadNum <= 32; threadNum *= 2) {
             start = omp_get_wtime();
+            int count = 0;
 
 
-            #pragma omp parallel for num_threads(threadNum) shared(arr) private(col) shared(row)
+            #pragma omp parallel for num_threads(threadNum) shared(arr) private(col) shared(row) private(a) private(b) shared(count)
             for (row = 0; row < n; row++) {
-                if (n == 300) {
-                    cout << "Thread #" << omp_get_thread_num() << " row #" << row << "\n";
+                count++;
+                if (n == 300 && omp_get_thread_num()>6) {
+                    //cout << "Thread #" << omp_get_thread_num() << " row #" << row << "\n";
                 }
                 for (col = 0; col < n; col++) {
 
@@ -38,11 +40,19 @@ int main(int argc, char* argv[]) {
                     b = (row * sqrt(sin(row)))/2;
 
                     arr[row] += a * b;
-                } 
+                }
             }
-
+            
 
             cout << "Time for " << threadNum << " threads - " << (omp_get_wtime() - start) << "s\n";
+            //cout << "Size " <<  sizeof(arr) << "\n";
+            //cout << "Count " <<  count << "\n";
+            
+
+            if (n == 300) { 
+                //for (int i=0; i<n; i++) printf("%d ", arr[i]);
+                cout << "\n"; 
+            }
         }
     }
     return 0;
