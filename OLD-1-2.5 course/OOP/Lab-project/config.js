b913334a0cc2e8268650d86c2ed15f5ee94e219f
OlehZherebetskiy <olehvel@gmail.com>
2021-03-20 20:44:35 +0200

    remove old bad commits

--- OLD-1-2.5 course/OOP/Lab-project/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    MONGO_URL: 'mongodb://localhost/noproc',
-    MONGO_URL_Token: 'mongodb://localhost/noprocT',
-    secret: 'verysicret'
-}
